b1ae6c589cfb847662db2583c15c2acdae5f659b
YannickGbaka <gosseyannick553@gmail.com>
2022-07-26 15:38:07 +0000

    Creating the api functions for the crud operations on the todo resource

--- app/Http/Controllers/Api/TodoController.php
+++ app/Http/Controllers/Api/TodoController.php
@@ -6,6 +6,7 @@ use App\Models\Todo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TodoResource;
+use Illuminate\Support\Facades\Validator;
 
 class TodoController extends Controller
 {
@@ -24,9 +25,25 @@ class TodoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $rules = [
+            'task' => 'required|string|min:3',
+            'priority' => 'required'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        Todo::create([
+            'task' => $request->task,
+            'priority' => $request->priority
+        ]);
+
+        return response()->json(['body' => 'La tâche a bien été crée']);
     }
 
     /**
@@ -57,9 +74,47 @@ class TodoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($id, Request $request)
     {
-        //
+        $rules = [
+            'task_id' => 'required',
+            'task' => 'required|string|min:3',
+            'priority' => 'required',
+            'state' => 'required|min:0|max:1'
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 401);
+        }
+
+        $todo = Todo::findOrFail($request->task_id);
+        $todo->task = $request->task;
+        $todo->priority = $request->priority;
+        $todo->state = $request->state;
+
+        $todo->save();
+
+        return response()->json(['body' => 'La tâche a bien été mis à jour']);
+    }
+
+    public function completeTask(Request $request)
+    {
+        $rules = [
+            'task_id' => 'required|int',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
+        $todo = Todo::findOrFail($request->task_id);
+        $todo->state = !$todo->state;
+        $todo->save();
+
+        return back()->with('task_updating', 'La tâche a été mise à jour avec succès');
     }
 
     /**
@@ -80,8 +135,11 @@ class TodoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        $todo = Todo::findOrFail($request->id);
+        $todo->delete();
+
+        return response()->json(['body' => 'Tâche supprimé avec succès']);
     }
 }
